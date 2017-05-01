ba6aefa16a2a20cff8bc1f51b0b677677c549935
Rajesh Kumar Aggani <raaggani@microsoft.com>
2017-05-02 01:08:54 +0530

    Add project files.

---
+++ ASPNetcoreServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace ASPNetcoreServer
+{
+    class Program
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = context =>
+                {
+                    context.Context.Response.Headers.Add("content-disposition", "attachment; a.dll1");
+                }
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
